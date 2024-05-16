5882dca476926f5e0f4a7088ef2d81f60e6a2f31
kennyChukwuebuka <kenkinado@gmail>
2024-05-16 17:08:34 +0100

    added update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,12 +10,31 @@ Write a function called filter_datum that returns the log message obfuscated
 
 
 import re
+import logging
 
 
 def filter_datum(fields, redaction, message, separator):
-    """filter_datum"""
-    pattern = (
-        r'(?:(?<=^)|(?<={0}))(?:{1}=)([^{1}{2}]+)'
-        .format(separator, '|'.join(fields), separator)
-    )
-    return re.sub(pattern, redaction, message)
+    pattern = r'({0}=)([^{0}]+)'.format('|'.join(fields))
+    return re.sub(pattern, r'\1' + redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: list[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record_dict = record.__dict__
+        for field in self.fields:
+            record_dict[field] = self.filter_datum(record_dict[field])
+        return super().format(record)
+
+    @staticmethod
+    def filter_datum(data: str) -> str:
+        return RedactingFormatter.REDACTION
