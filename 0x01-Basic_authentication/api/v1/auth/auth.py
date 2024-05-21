d96890f8c684ab062cce676e610ccace724fba70
kennyChukwuebuka <kenkinado@gmail>
2024-05-21 21:35:33 +0100

    added task

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,23 +3,20 @@
 """
 # File: api/v1/auth/auth.py
 
-# File: api/v1/auth/auth.py
-
-from typing import List, Optional
+from typing import List, TypeVar
 from flask import request
-
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Determines if authentication is required for a given path.
         :param path: The path to check.
-        :param excluded_paths: A list of req
+        :param excluded_paths: A list of paths that do not require authentication.
         :return: False for now. Logic to be implemented later.
         """
         return False
 
-    def authorization_header(self, request=None) -> Optional[str]:
+    def authorization_header(self, request=None) -> str:
         """
         Returns the authorization header from the request.
         :param request: The Flask request object.
@@ -27,7 +24,7 @@ class Auth:
         """
         return None
 
-    def current_user(self, request=None) -> Optional[object]:
+    def current_user(self, request=None) -> TypeVar('User'):
         """
         Returns the current user based on the request.
         :param request: The Flask request object.
