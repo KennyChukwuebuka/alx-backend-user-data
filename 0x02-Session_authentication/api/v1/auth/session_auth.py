41aa41a08e92f7654157932804e0adba351f3c71
kennyChukwuebuka <kenkinado@gmail>
2024-05-22 20:21:15 +0100

    updated ssesion auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@ Module of Session Authentication
 
 from api.v1.auth.auth import Auth
 import uuid
+from typing import TypeVar
 
 
 class SessionAuth(Auth):
@@ -20,3 +21,17 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Returns a User ID based on a Session ID
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Returns the current user
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return self.user_object_from_id(user_id)
