a9a1e574a29dc0159c01e813bf438d6a81f1cae7
kennyChukwuebuka <kenkinado@gmail>
2024-05-28 21:44:24 +0100

    updated user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,3 +35,14 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Finds user by kwargs
+            Return: User Object
+        """
+        if not kwargs:
+            raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
