3e083508a8d6c238e0ceeb5e60867fbf30879e8d
kennyChukwuebuka <kenkinado@gmail>
2024-05-28 20:10:12 +0100

    added db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -9,11 +9,19 @@ Contains the class DB
     return user object and save user to the database
 """
 
+import logging
+from typing import Dict
+
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
+
 from user import Base, User
+
+logging.disable(logging.WARNING)
 
 
 class DB:
@@ -49,3 +57,40 @@ class DB:
             self._session.rollback()
             raise
         return user
+
+    def find_user_by(self, **kwargs: dict[str, str]) -> User:
+        """Find a user by the given arguments
+        """
+        session = self._session
+        try:
+            user = session.query(User).filter_by(**kwargs).first()
+        except NoResultFound:
+            raise NoResultFound()
+        except InvalidRequestError:
+            raise InvalidRequestError()
+        # print("Type of user: {}".format(type(user)))
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user in the database
+        """
+        try:
+            # Find the user with the given user ID
+            user = self.find_user_by(id=user_id)
+        except NoResultFound:
+            raise ValueError("User with id {} not found".format(user_id))
+
+        # Update user's attributes
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                # Raise error if an argument that does not correspond to a user
+                # attribute is passed
+                raise ValueError("User has no attribute {}".format(key))
+            setattr(user, key, value)
+
+        try:
+            # Commit changes to the database
+            self._session.commit()
+        except InvalidRequestError:
+            # Raise error if an invalid request is made
+            raise ValueError("Invalid request")
