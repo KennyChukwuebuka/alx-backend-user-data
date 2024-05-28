fdddd814cd4ec7b796ecd0d3704d3dd4ef25f035
kennyChukwuebuka <kenkinado@gmail>
2024-05-28 21:48:05 +0100

    udated user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -46,3 +46,13 @@ class DB:
         if user is None:
             raise NoResultFound
         return user
+    
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ Updates user
+            Return: None
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+        self._session.commit()
