9aa16db4a2eaaeb19c7290658319bbc6bd20eb27
kennyChukwuebuka <kenkinado@gmail>
2024-05-29 14:22:50 +0100

    added auth system

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,8 @@
 
 import hashlib
 import bcrypt
+from db import DB
+from sqlalchemy.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -12,3 +14,20 @@ def _hash_password(password: str) -> bytes:
     hashed with bycrypt.hashpw
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> None:
+        """Register a user
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            self._db.add_user(email, _hash_password(password))
