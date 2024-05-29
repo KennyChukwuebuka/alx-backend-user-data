92eaa1adfe1ec77a19341f8c90604b67faf3fbcf
kennyChukwuebuka <kenkinado@gmail>
2024-05-29 18:00:27 +0100

    updated the app file with user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,9 +3,11 @@
 """
 from flask import Flask, jsonify
 from flask import request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -16,6 +18,26 @@ def home():
         "message": "Bienvenue"
     }
     return jsonify(reponse_payload)
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    """ Create a new user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        reponse_payload = {
+            "email": email,
+            "message": "user created"
+        }
+        return jsonify(reponse_payload)
+    except Exception:
+        reponse_payload = {
+            "message": "email already registered"
+        }
+        return jsonify(reponse_payload), 400
 
 
 if __name__ == "__main__":
